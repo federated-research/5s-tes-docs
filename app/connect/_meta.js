400c2295fcf54c1ef93dbcd853135aa502a49069
AndrewThien <anretrithien@gmail.com>
2026-07-27 14:00:37 +0100

    add connecting egress

--- app/connect/_meta.js
+++ app/connect/_meta.js
@@ -2,4 +2,5 @@ export default {
   "setup-tre": "Setup TRE Agent",
   approvals: "Approve Projects",
   "connect-submission": "Connecting a TRE",
+  "connect-egress": "Connect TRE to Data Egress",
 };
