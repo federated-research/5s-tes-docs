9ca268002a3d6e9aa65b7dd80a699d9ff97e42ac
Benjamin Partridge <bp1183@my.bristol.ac.uk>
2026-07-27 13:29:45 +0100

    fix: reverted setup-TRE back to setup-tre

--- app/connect/_meta.js
+++ app/connect/_meta.js
@@ -1,5 +1,5 @@
 export default {
-  "setup-TRE": "Setup TRE Agent",
+  "setup-tre": "Setup TRE Agent",
   approvals: "Approve Projects",
   "controlling-tre-data-access": "Controlling TRE Data Access", 
   "connect-submission": "Connecting a TRE"
