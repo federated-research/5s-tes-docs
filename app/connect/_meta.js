d445ae09c138c8c5e82c5d18947e944fa1fc3b1f
AndrewThien <anretrithien@gmail.com>
2026-07-27 16:30:25 +0100

    add comma

--- app/connect/_meta.js
+++ app/connect/_meta.js
@@ -3,6 +3,6 @@ export default {
   "connect-submission": "Connecting a TRE",
   approvals: "Approve Projects",
   "connect-submission": "Connecting a TRE",
-  "approve-outputs":"Review Outputs"
+  "approve-outputs":"Review Outputs",
   "tes-backend": "TES Backend",
 };
