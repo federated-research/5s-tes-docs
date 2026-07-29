da2dd95dc6b2187f5961e0201e7a6c9e407f5987
Jon Couldridge <jonathan.couldridge@nottingham.ac.uk>
2026-07-29 10:28:23 +0100

    fixing merge conflict error

--- app/_meta.js
+++ app/_meta.js
@@ -7,7 +7,7 @@ export default {
   // Welcome Report URL redirect, never show in nav
   five_safes_tes: {
     display: "hidden"
-  }
+  },
 
   // exclude routes until content published
   "co-ordinate": {
