71b5e1da535789428a41060c803a936afebc2762
AndrewThien <anretrithien@gmail.com>
2026-07-30 16:51:19 +0100

    add learn to the main metadata tree

--- app/_meta.js
+++ app/_meta.js
@@ -1,9 +1,10 @@
 export default {
+  learn: "Learn",
   connect: "Connect",
   "co-ordinate": "Co-ordinate",
 
   // Welcome Report URL redirect, never show in nav
   five_safes_tes: {
-    display: "hidden"
-  }
+    display: "hidden",
+  },
 };
