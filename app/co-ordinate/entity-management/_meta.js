4e61abab06cfababc91f3a8361d52a0f8a9c60b9
AndrewThien <anretrithien@gmail.com>
2026-07-31 18:51:32 +0100

    structure the pages

--- app/co-ordinate/entity-management/_meta.js
+++ app/co-ordinate/entity-management/_meta.js
@@ -1,3 +1,8 @@
 export default {
   "add-realm-admin": "Add a Realm admin",
+  "add-keycloak-user": "Add a Keycloak user", // Hidden from navigation for now because it not belongs to any card
+  "add-new-researcher": "Add a new Researcher",
+  "register-a-tre": "Register a new TRE",
+  "setup-new-project": "Setup a new Project",
+  "assign-tre-user-to-project": "Assign TRE and Researcher to a Project",
 };
