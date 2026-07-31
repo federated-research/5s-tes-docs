49c47dc6ce01c6869fdde1570ea0fa05709345f0
Jon Couldridge <beforan@jon.gg>
2026-07-31 14:39:48 +0100

    Under construction (#15)
    * added under construction component, so landing page links can be clearer instead of 404
    
    * added PR template, mainly to help ensure metadata inclusion
    
    * started adding under construction to non-existent linked pages, and excluding them from nav
    
    * fixing merge conflict error
    
    * updating existing component filenames to PascalCase
    
    * fixing call to action link icons and display
    
    * journey structure and landing page link updates
    
    * loading page placeholders

--- app/_meta.js
+++ app/_meta.js
@@ -1,9 +1,17 @@
 export default {
-  connect: "Connect",
-  "co-ordinate": "Co-ordinate",
+  index: {
+    display: "hidden"
+  },
 
   // Welcome Report URL redirect, never show in nav
   five_safes_tes: {
     display: "hidden"
-  }
+  },
+
+  // exclude routes until content published
+  "journeys": {
+    display: "hidden"
+  },
+
+  // TODO: restructure content and move "connect" journey page
 };
