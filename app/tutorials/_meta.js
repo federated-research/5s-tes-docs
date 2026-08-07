32e9052d045aa9dae837f03f3ba496b82aed780f
AndrewThien <anretrithien@gmail.com>
2026-08-07 16:58:57 +0100

    initial add hello world

--- app/tutorials/_meta.js
+++ app/tutorials/_meta.js
@@ -1,4 +1,5 @@
 export default {
+  "hello-world": "Send the first TES message locally",
   "submit-to-5s-tes": "Submit an analysis to 5S-TES",
   "collecting-results": "Collecting analysis results",
 };
