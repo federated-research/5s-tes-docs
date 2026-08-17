2562101f3cf8694084b091b2793fe6564c3bcbca
AndrewThien <anretrithien@gmail.com>
2026-08-17 16:13:11 +0100

    fix text

--- app/explanations/concepts/_meta.js
+++ app/explanations/concepts/_meta.js
@@ -1,5 +1,5 @@
 export default {
   "five-safes": "Five Safes framework",
-  tes: "Task Execution Service (TES)",
-  omop: "Observational Medical Outcomes Partnership (OMOP)",
+  tes: "TES",
+  omop: "OMOP",
 };
