89009788daea25767fac95eae7322b6fb839793d
Tri Thien Nguyen <119530400+AndrewThien@users.noreply.github.com>
2026-08-20 16:35:36 +0100

    Add banner, metadata and logo (#48)
    * add logo, banner and metadata
    
    * coloring
    
    * wording
    
    * metadata

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,19 +1,73 @@
 import { Footer, Layout, Navbar } from "nextra-theme-docs";
-import { Head } from "nextra/components";
+import { Banner, Head } from "nextra/components";
 import { getPageMap } from "nextra/page-map";
 import "nextra-theme-docs/style.css";
 import "./globals.css";
 import { FooterLogo } from "@/components/FooterLogo";
+import Image from "next/image";
 
 export const metadata = {
-  // Define your metadata here
-  // For more information on metadata API, see: https://nextjs.org/docs/app/building-your-application/optimizing/metadata
+  title: "Five Safes TES Documentation",
+  openGraph: {
+    siteName: "Five Safes TES Documentation",
+    type: "website",
+    locale: "en_UK",
+  },
+  keywords: [
+    "five safes",
+    "five safes TES documentation",
+    "TES",
+    "five safes TES",
+  ],
+  robots: {
+    index: true,
+    follow: true,
+    nocache: false,
+    googleBot: {
+      index: true,
+      follow: true,
+      noimageindex: false,
+      "max-video-preview": -1,
+      "max-image-preview": "large",
+      "max-snippet": 160,
+    },
+  },
+  description: "Five Safes TES Documentation",
+  manifest: "/meta/site.webmanifest",
+  icons: {
+    icon: [
+      {
+        url: "/meta/favicon.ico",
+        type: "image/x-icon",
+      },
+      {
+        url: "/meta/favicon-16x16.png",
+        sizes: "16x16",
+        type: "image/png",
+      },
+    ],
+    shortcut: [
+      {
+        url: "/meta/favicon.ico",
+        type: "image/x-icon",
+      },
+    ],
+    apple: [
+      {
+        url: "/meta/apple-touch-icon.png",
+        sizes: "180x180",
+        type: "image/png",
+      },
+    ],
+  },
 };
 
 const navbar = (
   <Navbar
-    logo={<b>Five Safes TES Docs</b>}
-    // ... Your additional navbar options
+    logo={<div className="flex items-center gap-2">
+      <Image src="/logo.svg" alt="logo" width={50} height={50} />
+      <b>Five Safes TES</b>
+    </div>}
   />
 );
 const footer = (
@@ -41,6 +95,11 @@ export default async function RootLayout({
         {/* Your additional tags should be passed as `children` of `<Head>` element */}
       </Head>
       <body>
+        <Banner storageKey="weave-docs-banner">
+          <a href="https://docs.federated-research.com/" target="_blank">
+            ❗ Documentation for Pattern and Weave has been moved to the new site❗ Go there →
+          </a>
+        </Banner>
         <Layout
           navbar={navbar}
           pageMap={await getPageMap()}
